fd7cc5c546a24c76d1bea7d8cdb5bab031148b6c
slin480 <lysoll8444@gmail.com>
2024-09-20 19:56:05 -0500

    commit2

--- TicketSystem.java
+++ TicketSystem.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TicketSystem {
     
     static void purchaseTicket(Visitor visitor){
 
-      Scanner obj1Scanner = new Scanner(System.in);
+      Scanner objScanner = new Scanner(System.in);
+
+      System.out.println("What is your name?");
+      String name = objScanner.next();
+      visitor.name = name;
 
       System.out.println("What is your age?");
-      int age = obj1Scanner.nextInt();
+      int age = objScanner.nextInt();
       visitor.age = age;
 
       if (visitor.age > 0 && visitor.age < 18){
@@ -15,28 +22,48 @@ public class TicketSystem {
         System.out.println("Person is " + visitor.age + " years old");
         System.out.println("Person receives " + visitor.ticket + " ticket");
       } 
+
       else if (visitor.age >= 18 && visitor.age < 60){
         visitor.ticket = visitor.ticketType[1];
         System.out.println("Success 2");
         System.out.println("Person is " + visitor.age + " years old");
         System.out.println("Person receives " + visitor.ticket + " ticket");
       } 
+
       else if (visitor.age >= 60 && visitor.age <= 100){
         visitor.ticket = visitor.ticketType[2];
         System.out.println("Success 3");
         System.out.println("Person is " + visitor.age + " years old");
         System.out.println("Person receives " + visitor.ticket + " ticket");
       } 
+
       else {
         System.out.println("Invalid Age. \nExiting Program");
         System.exit(0);
       }
 
-      obj1Scanner.close();
+      
+    }
+
+    static void purchaseTicket2(ArrayList<Visitor> visitor, int visitorNum){
+
+      Scanner objScanner = new Scanner(System.in);
+
+      for(int i = 0; i < visitorNum; i++){
+        
+      }
+
+      
     }
 
     static void joinRideQueue(Visitor visitor, String rideName){
-      
+      Scanner objScanner = new Scanner(System.in);
+
+      System.out.println("Which ride for? " + visitor.name + " (Please Answer with Corresponding Number) \n1. Ferris Wheel \n2. Roller Coaster \n3. Dropper");
+      int rideChoice = objScanner.nextInt();
+
+      System.out.println(visitor.name + " has selected " + visitor.ridePreference[rideChoice - 1]);
+      // obj2Scanner.close();
 
     }
 
@@ -47,8 +74,15 @@ public class TicketSystem {
 
 
     public static void main(String[] args) {
-    Visitor objVisitor = new Visitor();
-    purchaseTicket(objVisitor);
+    Random randNum = new Random();
+    int numOfVisitors = randNum.nextInt(5) + 1; //Generate random numbers between 1-5
+    System.out.println("You have " + numOfVisitors + " visitors");
+    ArrayList<Visitor> visitors = new ArrayList<Visitor>(numOfVisitors); //Create list of type Visitor 
+    
+    // purchaseTicket2(visitors);
+    // Visitor objVisitor = new Visitor();
+    // purchaseTicket(objVisitor);
+    // joinRideQueue(objVisitor, "Hello");
   }
   
 }
--- Visitor.java
+++ Visitor.java
@@ -1,7 +1,56 @@
 public class Visitor {
-	String Name;
+
+  String name;
   int age;
   String[] ticketType = {"Child", "Adult", "Senior"};
   String ticket;
   String[] ridePreference = {"Ferris Wheel", "Roller Coaster", "Dropper"};
+  String ride;
+
+  //Constructor
+  public Visitor(String name, int age, String ticket, String ride){
+    this.name = name;
+    this.age = age;
+    this.ticket = ticket;
+    this.ride = ride;
+  }
+
+  //Setters
+  public void setName(){
+  };
+
+  public void setAge(){
+  };
+
+  public void setTicket(){
+  };
+
+  public void setRide(){
+  };
+
+  //Getters
+  public String getName(){
+    return name;
+  };
+
+  public int getAge(){
+    return age;
+  };
+
+  public String getTicket(){
+    return ticket;
+  };
+
+  public String getRide(){
+    return ride;
+  };
+
+  //General Form
+  public void setDetails(){
+
+  }
+
+  public void getDetails(){
+
+  }
 }
